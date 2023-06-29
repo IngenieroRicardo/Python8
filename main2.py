943df1cc3150d180c553d8c2f8fe87859466a715
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-06-29 10:01:15 -0600

    Add files via upload

--- main2.py
+++ main2.py
@@ -5,4 +5,5 @@ ltcursor = ltconn.cursor()
 
 #ltcursor.execute("DELETE FROM tiposdocumentos_terrestre WHERE CodigoTipoDocumentoIdentidad=1000")
 ltcursor.execute("INSERT INTO tiposdocumentos_terrestre VALUES(1000, 'PASAPORTE PROVICIONAL');")
+ltconn.commit()
 ltcursor.close()
